2dcbb874f687d6e69de74277f735413391850854
devldavydov <devldavydov@yandex.ru>
2024-06-06 19:54:35 +0300

    Upd navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/common/Navbar";
+import Navbar, { ILink } from "./components/common/Navbar";
 import { Container } from "react-bootstrap";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      <Navbar
+        brand={{
+          name: "MyFood",
+          to: "/",
+        }}
+        links={[
+          {
+            name: "Статистика",
+            to: "/stats",
+          },
+          {
+            name: "Еда",
+            to: "/food",
+          },
+          {
+            name: "Журнал",
+            to: "/journal",
+          },
+          {
+            name: "Вес",
+            to: "/weight",
+          },
+          {
+            name: "Настройки",
+            to: "/settings",
+          },
+        ]}
+      />
       <Container>
         <div className="mb-3"></div>
         <Outlet />
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,7 +1,17 @@
 import { Link, NavLink } from "react-router-dom";
 import { Container, Nav, Navbar as NavbarRB } from "react-bootstrap";
 
-export default function Navbar() {
+export interface ILink {
+  name: string;
+  to: string;
+}
+
+export interface INavbarProps {
+  brand: ILink;
+  links: ILink[];
+}
+
+export default function Navbar({ brand, links }: INavbarProps) {
   return (
     <NavbarRB
       collapseOnSelect
@@ -12,28 +22,18 @@ export default function Navbar() {
     >
       <Container fluid>
         <NavbarRB.Brand>
-          <Link className="navbar-brand" to="/">
-            MyFood
+          <Link className="navbar-brand" to={brand.to}>
+            {brand.name}
           </Link>
         </NavbarRB.Brand>
         <NavbarRB.Toggle aria-controls="responsive-navbar-nav" />
         <NavbarRB.Collapse id="responsive-navbar-nav">
           <Nav>
-            <NavLink to="/stats" className="nav-link">
-              Статистика
-            </NavLink>
-            <NavLink className="nav-link" to="/food">
-              Еда
-            </NavLink>
-            <NavLink className="nav-link" to="/journal">
-              Журнал
-            </NavLink>
-            <NavLink className="nav-link" to="/weight">
-              Вес
-            </NavLink>
-            <NavLink className="nav-link" to="/settings">
-              Настройки
-            </NavLink>
+            {links.map((l) => (
+              <NavLink key={l.to} className="nav-link" to={l.to}>
+                {l.name}
+              </NavLink>
+            ))}
           </Nav>
         </NavbarRB.Collapse>
       </Container>
